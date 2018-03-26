ce3bbfd51a817e9b83014bb615957a2141cede5c
fajfara <a.fajfar95@gmail.com>
2018-03-26 19:34:36 +0200

    Updating gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,21 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
 
 gulp.task('html', function(){
   console.log("Changes have been made to the html file!");
 });
 
 gulp.task('styles', function(){
-  console.log("Changes have been made to the css file!");
+  return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/assets/temp/styles'));
 });
 
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
-  watch('./app/index.html', function(){
+  gulp.watch('./app/index.html', function(){
     gulp.start('html');
   });
 
-  watch('./app/assets/styles/*.css'), function(){
+  gulp.watch('app/assets/styles/**/*.css'), function(){
     gulp.start('styles');
   };
 
