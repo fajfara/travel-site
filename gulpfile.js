5f83549bbd264449da430d83640f70aea549d4d2
fajfara <a.fajfar95@gmail.com>
2018-03-29 18:44:38 +0200

    Going on throught the tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
-gulp.task('html', function(){
-  console.log("Changes have been made to the html file!");
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('styles', function(){
-  return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/assets/temp/styles'));
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport ,cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
 
 gulp.task('watch', function() {
 
-  gulp.watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     gulp.start('html');
   });
 
-  gulp.watch('app/assets/styles/**/*.css'), function(){
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles');
-  };
+  });
 
 });
